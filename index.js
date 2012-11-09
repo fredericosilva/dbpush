cedc83cb6f7de28924939cb4a01602d631686f02
Frederico Silva <frederico.silva@gmail.com>
2012-11-09 13:33:52 +0100

    add module code

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var tedious = require('tedious'),
+    TYPES   = tedious.TYPES;
+
+module.exports = function(connectionData, spMapping, callback) {
+
+    connectionData.options = {
+        connectTimeout : 1000,
+        requestTimeout : 1000,
+        cancelTimeout  : 1000
+    };
+
+    var connection = new tedious.Connection(connectionData);
+
+    connection.on('connect', function(err) {
+        if(err) {
+            console.log(err);
+            connection.close();
+        } else {
+            callback(err, invoker);
+        }
+    });
+
+    function invoker(spName, map) {
+        spName = spMapping[spName] || spName;
+
+        var sqlRequest = new tedious.Request(spName, function(err) {
+            if (err) {
+                console.log(err);
+            }
+            connection.close();
+        });
+
+        for(var key in map) {
+            sqlRequest.addParameter(key, TYPES.VarChar, map[key]);
+        }
+
+        connection.callProcedure(sqlRequest);
+    }
+
+};
